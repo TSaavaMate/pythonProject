ba87c8d577771489f0db532cdb7cf2a2cd0d7075
Mate <you@example.com>
2022-12-23 18:18:11 +0400

    comit init

--- mockup 3/#1.py
+++ mockup 3/#1.py
@@ -1,4 +1,4 @@
-def sumOfEven(list):
+def sumOfEven(List):
     # count=0
     # for i in list:
     #     if i%2==0:
@@ -6,7 +6,9 @@ def sumOfEven(list):
     # if count==0:
     #     return -1
     # return count
-    return -1 if sum(i for i in list if i%2==0)==0 else sum(i for i in list if i%2==0)
-print(sumOfEven([4, 2, 12 ]))
-print(sumOfEven([3, -1, -7, -9 ]))
+    return -1 if sum(i for i in List if i % 2 == 0) == 0 else sum(i for i in List if i % 2 == 0)
+
+
+print(sumOfEven([4, 2, 12]))
+print(sumOfEven([3, -1, -7, -9]))
 print(sumOfEven([4, -6, 9, 8, 1]))
--- mockup 3/#3.py
+++ mockup 3/#3.py
@@ -1,11 +1,13 @@
-def sumOfMatrixDiagonal(list):
+def sumOfMatrixDiagonal(List):
     # count=0
     # for i,j in zip(range(len(list)),reversed(range(len(list)))):
     #     count+=list[i][j]
     # return count
-    return sum(list[i][j] for i,j in zip(range(len(list)),reversed(range(len(list)))))
+    return sum(List[i][j] for i, j in zip(range(len(List)), reversed(range(len(List)))))
+
+
 print(sumOfMatrixDiagonal(
-    [[1, 2, (3)],
-     [4, (5), 6],
-     [(7), 8, 9]]
+    [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
 ))
